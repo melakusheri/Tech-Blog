de1ca6bf466c3653325c6132d40190ddbf774e4b
Melaku Sheri <mailangels@gmail.com>
2021-09-25 11:45:30 -0400

    models and puplic

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
 //import sequelize
 const Sequelize = require("sequelize");
 
-require("dotenv").config(); // For loading env variables
+require("dotenv").config(); 
+// For loading env variables
 //create the connection to the database
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
@@ -12,22 +13,7 @@ const sequelize = process.env.JAWSDB_URL
         decimalNumbers: true,
       },
     });
-// if (process.env.JAWSDB_URL) {
-//   //we have jawsdb
-//   console.log("We have and will use jawsdb!");
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   //otherwise connect to local db
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME, //vars set up in .env
-//     process.env.DB_USER,
-//     process.env.DB_PW,
-//     {
-//       host: "localhost",
-//       dialect: "mysql",
-//       port: 3306,
-//     }
-//   );
-// }
+
+    
 
 module.exports = sequelize;
